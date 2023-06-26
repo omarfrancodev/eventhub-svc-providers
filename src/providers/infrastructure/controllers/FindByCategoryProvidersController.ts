528fcb9f9ad6ac43e4f3509ca05783c900c575ca
Omar Franco <fofe2803@gmail.com>
2023-06-26 17:52:41 -0600

    feat (provider): [controller] add the console log error

--- src/providers/infrastructure/controllers/FindByCategoryProvidersController.ts
+++ src/providers/infrastructure/controllers/FindByCategoryProvidersController.ts
@@ -16,6 +16,7 @@ export class FindByCategoryProviderController {
 
       return res.status(200).json(providers);
     } catch (error) {
+      console.error(error);
       return res.status(500).json({ error: 'Internal server error' });
     }
   }
